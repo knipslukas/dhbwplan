a3abbbb3ffafbf06c4590388763634b19241d743
knipslukas <58523569+knipslukas@users.noreply.github.com>
2019-12-05 12:00:17 +0100

    Initial Commit
    Einrichtung eines Spring Projekts

---
+++ src/main/java/de/amc17/dhbwplan/DhbwplanApplication.java
@@ -0,0 +1,13 @@
+package de.amc17.dhbwplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhbwplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhbwplanApplication.class, args);
+	}
+
+}
