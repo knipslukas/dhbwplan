8c93e587346d355a6bf24cf1fde7f28c2249af40
Knips, Lukas <Lukas.Knips@transgourmet.de>
2020-03-02 15:39:00 +0100

    Update Dozent.java

--- src/main/java/de/amc17/dhbwplan/entity/Dozent.java
+++ src/main/java/de/amc17/dhbwplan/entity/Dozent.java
@@ -1,7 +1,6 @@
 package de.amc17.dhbwplan.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.GenericGenerator;
+import de.amc17.dhbwplan.enums.Zeitpraeferenzen;
 
 @Entity
 @Table(name = "dozent")																				
@@ -20,104 +19,108 @@ public class Dozent implements Serializable {
 	@Column(name="DID")
 	private int DID;
 	
-	@Column(name="Vorname")
-	private String vorname;
-	
-	@Column(name="Nachname")
-	private String nachname;
-	
-	@Column(name="Email")
-	private String email;
-	
-    @Column(name="Anrede") 							//TODO: Umbenennen restlicher Attribute
 	private String anrede;
     
-    private String Lala;
-	
-	@Column(name="Titel")
 	private String titel;
+	
+	private String vorname;
+	
+	private String nachname;
+	
+	private String unternehmen;
+	
+	private boolean intern;
+	
+	private String strassehaus;
+	
+	private String plz;
+	
+	private String stadt;
+	
+	private String email;
 	
 	private String telefonnummer;
 	
-//	@Column(name="Hauptamtlich") 
-//	private boolean mHauptamtlich;
-	//private ArrayList<Fach> moeglicheFaecher;
+	private String unterrichtet;
 	
-	//Funktioniert so nicht, Abbildung so nicht moeglich
-	//private ArrayList<Fach> gehalteneFaecher;
-	@Column(name="Kommentar")
-	private String mKommentar;
+	private String schwerpunkt;
+	
+	private Zeitpraeferenzen zeitpraef;
+	
+	private String notiz;
 
-//	  @ManyToMany(mappedBy="dozenten")
-//	  private Set<Modul> faecher;
-//	  private Set<Modul> gehalteneFaecher
-
-	public int getDID() {
-		return DID;
+	public String getTitel() {
+		return titel;
 	}
 
-	public void setDID(int aID) {
-		DID = aID;
+	public void setTitel(String titel) {
+		this.titel = titel;
 	}
 
-	public String getvorname() {  //getter, setter müssen den gleichen Namen wie die Variablen bekommen !!! 
+	public String getVorname() {
 		return vorname;
 	}
 
-	public void setvorname(String avorname) {
-		vorname = avorname;
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
 	}
 
-	public String getnachname()
-	{
+	public String getNachname() {
 		return nachname;
 	}
-	
-	public void setnachname(String aNachname)
-	{
-		nachname = aNachname;
-	}
-	
-	public String getemail()
-	{
-		return email;
-	}
-	
-	public void setemail(String aEmail)
-	{
-		email = aEmail;
-	}
-		
-	public String getAnrede()
-	{
-		return anrede;
-	}
-	
-	public void setAnrede(String aAnrede)
-	{
-		anrede = aAnrede;
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
 	}
 
-	public String getTitel()
-	{
-		return titel;
+	public String getUnternehmen() {
+		return unternehmen;
 	}
-	
-	public void setTitel(String aTitel)
-	{
-		titel = aTitel;
+
+	public void setUnternehmen(String unternehmen) {
+		this.unternehmen = unternehmen;
 	}
-	
-//	public boolean getHauptamtlich()
-//	{
-//		return mHauptamtlich;
-//	}
-//	
-//	public void setHauptamtlich(boolean aHauptamtlich)
-//	{
-//		mHauptamtlich = aHauptamtlich;
-//	}
-	
+
+	public boolean isIntern() {
+		return intern;
+	}
+
+	public void setIntern(boolean intern) {
+		this.intern = intern;
+	}
+
+	public String getStrassehaus() {
+		return strassehaus;
+	}
+
+	public void setStrassehaus(String strassehaus) {
+		this.strassehaus = strassehaus;
+	}
+
+	public String getPlz() {
+		return plz;
+	}
+
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+
+	public String getStadt() {
+		return stadt;
+	}
+
+	public void setStadt(String stadt) {
+		this.stadt = stadt;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getTelefonnummer() {
 		return telefonnummer;
 	}
@@ -126,36 +129,49 @@ public class Dozent implements Serializable {
 		this.telefonnummer = telefonnummer;
 	}
 
-	public String getKommentar()
-	{
-		return mKommentar;
+	public String getUnterrichtet() {
+		return unterrichtet;
 	}
-	
-	public void setKommentar(String aKommentar)
-	{
-		mKommentar = aKommentar;
-	}
-		
 
-//
-//		public Set<Modul> getfaecher() 
-//		{
-//			return faecher;
-//		}
-//
-//		public void setfaecher(Set<Modul> aFächer) 
-//		{
-//			faecher = aFächer;
-//		}
-//		
-//		public Set<Modul> getgehalteneFaecher() 
-//		{
-//			return gehaltenefaecher;
-//		}
-//
-//		public void setgehalteneFaecher(Set<Modul> aGehalteneFächer) 
-//		{
-//			gehalteneFaecher = aGehalteneFächer;
-//		}
+	public void setUnterrichtet(String unterrichtet) {
+		this.unterrichtet = unterrichtet;
+	}
+
+	public String getSchwerpunkt() {
+		return schwerpunkt;
+	}
+
+	public void setSchwerpunkt(String schwerpunkt) {
+		this.schwerpunkt = schwerpunkt;
+	}
+
+	public Zeitpraeferenzen getZeitpraef() {
+		return zeitpraef;
+	}
+
+	public void setZeitpraef(Zeitpraeferenzen zeitpraef) {
+		this.zeitpraef = zeitpraef;
+	}
+
+	public String getNotiz() {
+		return notiz;
+	}
+
+	public void setNotiz(String notiz) {
+		this.notiz = notiz;
+	}
+
+	public int getDID() {
+		return DID;
+	}
+
+	public String getAnrede() {
+		return anrede;
+	}
+
+	public void setAnrede(String anrede) {
+		this.anrede = anrede;
+	}
+
 	
 }
